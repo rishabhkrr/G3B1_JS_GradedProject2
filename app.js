9159160a60f9faa87a05f8a79e66c6180418dac1
Rishabh Kumar <work25860@gmail.com>
2024-01-28 18:12:39 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const username = 'admin';
+const password = 'admin';
+const form = document.getElementById('login-form');
+const errorMessage = document.getElementById('error-message');
+
+// Store the username and password in local storage
+localStorage.setItem('username', username);
+localStorage.setItem('password', password);
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const userInput = document.getElementById('username').value;
+    const passInput = document.getElementById('password').value;
+
+    if (userInput === username && passInput === password) {
+        window.location.href = 'resume.html';
+        alert('login Successfully');
+    } else {
+        errorMessage.innerText = 'Invalid username/password';
+    }
+});
+
+// Restrict user from going back to the login page
+window.onload = function () {
+    if (window.history.replaceState) {
+        window.history.replaceState(null, null, window.location.href);
+    }
+};
+
+window.onpopstate = function () {
+    window.history.replaceState(null, null, window.location.href);
+};
+
